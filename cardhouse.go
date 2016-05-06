a51c17d6861fc7827e87009f838fb6716aeff858
pratz <pratz0909@gmail.com>
2016-05-06 18:24:27 +0530

    Move to different vm

---
+++ cardhouse.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"github.com/codegangsta/cli"
+	"log"
+	"os"
+
+	"github.com/pratz/cardhouse/config"
+)
+
+// SupportedOS to build infrastructure
+var SupportedOS = []string{"centos:7.0", "ubuntu:14.04"}
+
+func isSupportedOS(distro string) (string, error) {
+
+	for _, os := range SupportedOS {
+		if distro == os {
+			return distro, nil
+		}
+	}
+
+	return distro, fmt.Errorf("%s distro not supported. Current supported distros %v", distro, SupportedOS)
+}
+
+func main() {
+
+	app := cli.NewApp()
+	app.Name = "CardHouse"
+	app.Version = "0.0.1"
+	app.Usage = "Dynamic infrastructure for ansible playbook"
+
+	app.Flags = []cli.Flag{
+		cli.StringSliceFlag{
+			Name:  "os",
+			Value: &cli.StringSlice{},
+			Usage: "Supported os images",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		if c.NArg() > 0 {
+			command := c.Args()[0]
+			fmt.Println(command)
+		} else {
+			cli.ShowAppHelp(c)
+			fmt.Println("Ansible playbook command is required")
+		}
+
+		selectedOS := c.StringSlice("os")
+		if len(selectedOS) <= 0 {
+			fmt.Println("Use flag --os to specify os type")
+		}
+
+		for _, osType := range selectedOS {
+			_, err := isSupportedOS(osType)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		fmt.Println(selectedOS)
+
+		// fix this to take relative path
+		userConfig, err := config.LoadConfig("/home/pratz/.cardhouse/config.yaml")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(userConfig.Remotes["default"])
+
+		return nil
+	}
+
+	app.Run(os.Args)
+
+}
